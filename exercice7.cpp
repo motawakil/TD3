14c36181989a537b80075153ad6dbe034a971422
motawakil <89648434+motawakil@users.noreply.github.com>
2023-11-04 21:59:18 +0100

    exercice7.cpp

--- exercice7.cpp
+++ exercice7.cpp
@@ -1,15 +1,15 @@
-/*
+
 1- C c1 : on applelons un object de class c1 qui herite du class A et B 
-1-1  : class B affiche : "** construction objet B : 1 0 car n = 1 et y = v = 0.0 
-1-2 : class A affiche : "** construction objet A : 1 1 car n= p = n1 = 1  et x= 1 
-1-3 : class C affiche : "** construction objet C : 3 3 car n = n3 = 3 et p = n1+n2 = 1+2
+1-1  : class B affiche : "** construction objet B : 1 0 ; car n = 1 et y = v = 0.0 
+1-2 : class A affiche : "** construction objet A : 1 1 ; car n= p = n1 = 1  et x= 1 
+1-3 : class C affiche : "** construction objet C : 3 3 ; car n = n3 = 3 et p = n1+n2 = 1+2
 
 2 - pour la declaration C c2(10, 11, 12, 5.0) 
-2-1 :  class B affiche : "** construction objet B : 1 5 car n = 1 et y = v = 5.0
-2-2 :  class A affiche : "** construction objet A : 10 1 car n = p = n1 = 10 et x = 1
-2-3 :  class C affiche : "** construction objet C : 12 21 car n = n3 = 12 et p= n1 + n2 = 10+11 = 21 
+2-1 :  class B affiche : "** construction objet B : 1 5 ; car n = 1 et y = v = 5.0
+2-2 :  class A affiche : "** construction objet A : 10 1 ; car n = p = n1 = 10 et x = 1
+2-3 :  class C affiche : "** construction objet C : 12 21 ; car n = n3 = 12 et p= n1 + n2 = 10+11 = 21 
 
-*/
+
 
 
 
